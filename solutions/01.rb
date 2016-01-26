e9d26bebe9c02f86ae950200049be6159ba6d4f3
Ognyan Angelov <sick.java.junkie@gmail.com>
2016-01-26 21:35:10 +0200

    Improve style: Naming, vars; Change return operator to <=>.

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,13 +1,14 @@
 def convert_to_bgn(amount, currency)
   currencies = {bgn: 1, usd: 1.7408, eur: 1.9557, gbp: 2.6415}
 
-  (amount * currencies[currency]).round(2)
+  converted_amount = amount * currencies[currency]
+  converted_amount.round(2)
 end
 
 
-def compare_prices(amount, currency, other_amount, other_currency)
-  amount_in_bgn = convert_to_bgn(amount, currency)
-  other_amount_in_bgn = convert_to_bgn(other_amount, other_currency)
+def compare_prices(first_amount, first_currency, second_amount, second_currency)
+  first_amount_in_bgn = convert_to_bgn(first_amount, first_currency)
+  second_amount_in_bgn = convert_to_bgn(second_amount, second_currency)
 
-  amount_in_bgn - other_amount_in_bgn
+  first_amount_in_bgn <=> second_amount_in_bgn
 end
