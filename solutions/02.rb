31377de42bb4af5c0062e2ce194ba61cdf4b361b
Ognyan Angelov <sick.java.junkie@gmail.com>
2016-01-26 23:07:19 +0200

    Extract inside? function; Utilize more of syntax.

--- solutions/02.rb
+++ solutions/02.rb
@@ -14,23 +14,28 @@ def grow(snake, direction)
   snake[0..-1].push move_segment(head, direction)
 end
 
+def inside?(point, rectangle)
+  within_x = point[0].between?(0, rectangle[:width] - 1)
+  within_y = point[1].between?(0, rectangle[:height] - 1)
+
+  within_x && within_y
+end
+
 def new_food(food, snake, dimensions)
-  x_spaces = (0..dimensions[:width] - 1).to_a
-  y_spaces = (0..dimensions[:height] - 1).to_a
+  x_spaces = (0...dimensions[:width]).to_a
+  y_spaces = (0...dimensions[:height]).to_a
+
   all_spaces = x_spaces.product y_spaces
   taken_spaces = food + snake
 
-  all_spaces.shuffle.find { |space| !taken_spaces.include? space }
+  (all_spaces - taken_spaces).sample
 end
 
 
 def obstacle_ahead?(snake, direction, dimensions)
   next_head = move_segment(snake[-1], direction)
 
-  within_x = next_head[0] >= 0 && next_head[0] < dimensions[:width]
-  within_y = next_head[1] >= 0 && next_head[1] < dimensions[:height]
-
-  !within_x || !within_y || (snake.include? next_head)
+  !inside?(next_head, dimensions) || (snake.include? next_head)
 end
 
 def danger?(snake, direction, dimensions)
